fc0b094755b0cf5ee56aa123a00eee48f8810e98
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2025-06-09 22:12:29 +0700

    up tiếp

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,7 +1,6 @@
-export * from './constants'
+export * from './constants.js'
 
 export {default as db} from './mongodb'
 export {default as cache} from './caching'
 export {default as logger} from './logger'
 export {default as mailTransporter} from './mail-transporter'
-
